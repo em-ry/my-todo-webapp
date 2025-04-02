610f18d527f3420fb55feec75c6b2b06d1ebd559
chuka <emry390@gmail.com>
2025-04-02 09:00:06 +0100

    fix bug of todo item replication, contain duplication todo keys with if statement

--- web.py
+++ web.py
@@ -6,8 +6,11 @@ todos = functions.get_todos()
 
 def add_todo():
     new_todo = st.session_state["new_todo"] + "\n"
-    todos.append(new_todo)
-    functions.write_todos(todos)
+    if new_todo not in todos:
+        todos.append(new_todo)
+        functions.write_todos(todos)
+    else:
+        st.warning("No duplicate todo items!!")
 
 
 st.title("My Todo App")
@@ -15,7 +18,7 @@ st.subheader("A minimalist todo app")
 
 # remove completed todos
 for index, todo in enumerate(todos):
-    # assign unique keys so as to reflect in session state.
+    # assign unique keys so it reflects in session state.
     checkbox = st.checkbox(todo, key=todo)
     if checkbox:
         todos.pop(index)
@@ -23,11 +26,9 @@ for index, todo in enumerate(todos):
         del st.session_state[todo]
         st.rerun()
 
-for todo in todos:
-    st.checkbox(todo)
-
-# the on_change attribute is like a callback function(takes a custom function)
+# the on_change attribute is a callback function(takes a custom function)
 st.text_input(label="Type your todo", label_visibility="hidden",
               placeholder="Add a todo...", on_change=add_todo,
               key="new_todo")
 
+st.session_state
